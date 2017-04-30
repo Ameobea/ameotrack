739d6b9c5342e9571e43b2f66b5871676e621515
Casey Primozic <me@ameo.link>
2017-04-30 19:52:53 -0400

    Small tweaks to feedback data processing

--- routes/feedback.js
+++ routes/feedback.js
@@ -13,14 +13,12 @@ const mailgun = require('mailgun-js')({apiKey: conf.mailgunKey, domain: conf.mai
  * message: Message containing the content of the feedback that will be included in the email
  */
 router.post('/', (req, res, next) => {
-  console.log(req.body);
   if(req.body.password == conf.feedbackPassword) {
     const data = {
       from: `AmeoTrack Feedback Sender <feedback@${conf.mailgunDomain}>`,
       to: conf.feedbackRecipient,
       subject: `${req.body.appName} Feedback`,
-      text: req.body.message,
-      html: req.body.message,
+      text: `Email: ${req.body.email}\n\nMessage:\n${req.body.message}`,
     };
 
     mailgun.messages().send(data, (err, body) => {
