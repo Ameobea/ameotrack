c0995fd557c207d41595a816e7c714143d5d0374
Casey Primozic <me@ameo.link>
2018-03-26 01:06:46 -0500

    Add markdown and HTML rendering options to bin

--- routes/bin.js
+++ routes/bin.js
@@ -1,11 +1,14 @@
+const path = require('path');
+
 const express = require('express');
 const router = express.Router();
+const showdown = require('showdown');
 
 const dbq = require('../helpers/dbQuery.js');
 
-router.get('/', (req, res, next) => {
-  res.render('bin_view', { text: '', filename: 'file.txt', back: '.' });
-});
+router.get('/', (req, res, next) =>
+  res.render('bin_view', { text: '', filename: 'file.txt', back: '.' })
+);
 
 router.post('/', (req, res, next) => {
   dbq.saveBin(
@@ -16,28 +19,28 @@ router.post('/', (req, res, next) => {
     req.body.secret,
     (err, dbRes) => {
       if (err) {
-        res.render('error');
-      } else {
-        res.render('redirect', {
-          url: './bin/' + dbRes.shortname,
-          filename: 'file.txt',
-        });
+        return res.render('error');
       }
+
+      res.render('redirect', {
+        url: './bin/' + dbRes.shortname,
+        filename: 'file.txt',
+      });
     }
   );
 });
 
 router.get('/:shortname', (req, res, next) => {
   dbq.getBin(req.params.shortname, (err, dbRes) => {
-    if (dbRes) {
-      res.render('bin_view', {
-        text: dbRes.text,
-        filename: dbRes.filename,
-        back: '..',
-      });
-    } else {
-      res.render('no_bin');
+    if (!dbRes) {
+      return res.render('no_bin');
     }
+
+    res.render('bin_view', {
+      text: dbRes.text,
+      filename: dbRes.filename,
+      back: '..',
+    });
   });
 });
 
@@ -50,16 +53,55 @@ router.post('/:shortname', (req, res, next) => {
     req.body.secret,
     (err, dbRes) => {
       if (err) {
-        res.render('bin_wrongPass');
-      } else {
-        res.render('bin_view', {
-          text: dbRes.text,
-          filename: dbRes.filename,
-          back: '..',
-        });
+        return res.render('bin_wrongPass');
       }
+
+      res.render('bin_view', {
+        text: dbRes.text,
+        filename: dbRes.filename,
+        back: '..',
+      });
     }
   );
+});
+
+const getBinRenderer = (ext, text) =>
+  ({
+    '.md': {
+      templateName: 'bin_render_markdown',
+      args: {
+        rendered: (() => {
+          const converter = new showdown.Converter();
+          converter.setFlavor('github');
+          converter.setOption('strikethrough', true);
+          return converter.makeHtml(text);
+        })(),
+      },
+    },
+    '.html': {
+      templateName: 'bin_render_html',
+      args: {
+        content: text,
+      },
+    },
+  }[ext]);
+
+router.get('/:shortname/rendered', (req, res, next) => {
+  dbq.getBin(req.params.shortname, (err, dbRes) => {
+    if (!dbRes) {
+      return res.render('no_bin');
+    }
+
+    const extension = path.extname(dbRes.filename);
+    const renderer = getBinRenderer(extension, dbRes.text);
+    if (!renderer) {
+      // Fall back to dumping raw text if no renderer available
+      res.set({ 'Content-Type': 'text/plain' });
+      res.send(dbRes.text);
+    } else {
+      res.render(renderer.templateName, renderer.args);
+    }
+  });
 });
 
 module.exports = router;
