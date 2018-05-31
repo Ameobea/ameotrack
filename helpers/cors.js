7dc0211e27d562b96d06ccab5322e80396440e45
Casey Primozic <me@ameo.link>
2018-05-31 17:33:50 -0400

    Fix code block styling for markdown rendering
     * Fix issue with the way CORS is set up to export the module correctly

--- helpers/cors.js
+++ helpers/cors.js
@@ -1,7 +1,7 @@
 /**
  * Express router middleware to add CORS headers.
  */
-exports.default = (req, res, next) => {
+module.exports = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header(
     'Access-Control-Allow-Headers',
